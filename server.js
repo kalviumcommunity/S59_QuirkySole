6e910b4925c211add9c1b4aefec997f5a047b435
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-02-27 17:04:19 +0530

    Linked the mongoDb to the Express file

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
 
 const port = 1111;
 
+const {connectToDB, isConnected} = require('./db')
+
+connectToDB()
+
 app.get('/', (req, res)=>{
-    res.send("Pong")
+    if(isConnected()){
+        res.send(`<h1>Your Database is connected</h1>`)
+    }
+    else{
+        // mongoose.disconnect()
+        res.send(`<h1>Your Database is not connected</h1>`)
+    }
 })
+
+
 
 app.listen(port, ()=> {
     console.log(`App is running on port ${port}`)
+    mongoose.connection.on('connected',()=>{
+        console.log('connected')
+    })
+    mongoose.connection.on('disconnected',()=>{
+        console.log('disconnected')
+    })
 })
+
+module.exports = {
+    app
+}
 
 // this is server.js
