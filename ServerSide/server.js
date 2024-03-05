3f99cac080396e6239a7c329279dfeb00921c1b8
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-03-05 09:46:59 +0530

    server.js update

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -1,34 +1,41 @@
-const routes = require('./Controller/routes');
 const express = require('express');
 const mongoose = require('mongoose')
-const app = express();
-const port = 1213
 const {connectToDB, isConnected} = require('./db')
-connectToDB()
-app.use(express.json());
 
+const cors = require('cors')
+const app = express();
+
+const port = 1213
+
+
+
+app.use(cors())
+const routes = require('./Controller/routes');
+app.use(express.json());
 // Mount routes
 app.use('/api', routes);
 
-app.get('/', (req, res)=>{
-    if(isConnected()){
-        res.send(`<h1>Your Database is connected</h1>`)
-    }
-    else{
-        res.send(`<h1>Your Database is not connected</h1>`)
-    }
-})
+connectToDB()
 
-app.listen(port, ()=> {
-    console.log(`App is running on port ${port}`)
-    mongoose.connection.on('connected',()=>{
-        console.log('connected')
+    app.get('/', (req, res)=>{
+        if(isConnected()){
+            res.send(`<h1>Your Database is connected</h1>`)
+        }
+        else{
+            res.send(`<h1>Your Database is not connected</h1>`)
+        }
     })
-    mongoose.connection.on('disconnected',()=>{
-            console.log('disconnected')
+
+    app.listen(port, ()=> {
+        console.log(`App is running on port ${port}`)
+        mongoose.connection.on('connected',()=>{
+            console.log('connected')
         })
-    })
+        mongoose.connection.on('disconnected',()=>{
+                console.log('disconnected')
+            })
+        })
 
-module.exports = {
-    app
-}
+    module.exports = {
+        app
+    }
